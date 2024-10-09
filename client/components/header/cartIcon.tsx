11a75442e9c9c8cf756b311809b7cba7cd1d04c4
Noel Garate Callejo <noel.garate@lastminute.com>
2024-10-09 18:52:00 +0200

    feat(client): add badge with the total product count

--- client/components/header/cartIcon.tsx
+++ client/components/header/cartIcon.tsx
@@ -1,9 +1,38 @@
 import styled from 'styled-components';
+import { useContext } from 'react';
+import { IconButton } from '@radix-ui/themes';
+import CartContext from '@lib/cartContext';
 
-const StyledImage = styled.img`
+const Button = styled(IconButton)`
+  position: relative;
   height: 26px;
+  background-color: var(--siphon);
+`;
+
+const StyledBadge = styled.div`
+  display: none;
+  ${(props) => props.totalItems > 0 && `display: block;`}
+  position: absolute;
+  right: -0.8em;
+  top: -0.8em;
+  min-width: 1.6em;
+  height: 1.6em;
+  border-radius: 0.8em;
+  background-color: red;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.8em;
+  color: var(--ice);
 `;
 
 export default function CartIcon() {
-  return <StyledImage src="/basket.svg" alt="Cart icon" />;
+  const { cartItems } = useContext(CartContext) as { cartItems: Map<string, number> };
+  const totalItems = Array.from(cartItems.values()).reduce((acc, item) => acc + item, 0);
+
+  return (
+    <Button>
+      <img src="/basket.svg" alt="Cart icon" />
+      <StyledBadge totalItems={totalItems}>{totalItems}</StyledBadge>
+    </Button>
+  );
 }
