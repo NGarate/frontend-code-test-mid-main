891d3f4c32cba4a52e7cba3a2f058156df252711
Noel Garate Callejo <noel.garate@lastminute.com>
2024-10-09 18:29:18 +0200

    feat(client): style addToCart

--- client/components/product/addToCart.tsx
+++ client/components/product/addToCart.tsx
@@ -3,35 +3,80 @@ import { Box, Button, Text } from '@radix-ui/themes';
 import { useContext, useState } from 'react';
 import CartContext, { CartItemsMap } from '@lib/cartContext';
 
-const StyledButton = styled(Button)`
-  width: 50px;
-  background-color: var(--siphon);
-  text-align: center;
-  margin-right: var(--space-2);
+
+const Container = styled(Box)`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-top: var(--space-4);
+`;
+const CounterContainer = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  gap: var(--space-2);
+`;
+const MinusIcon = styled(Button)`
+  &:after {
+    content: '-';
+  }
+  ${(props) =>
+    props.disabled
+      ? 'background-color: var(--plum); color: var(--purpleHaze);'
+      : 'background-color: var(--sohoLights); color: var(--hemocyanin);'}
+  height: var(--space-6);
+  width: var(--space-6);
+  border-radius: 33%;
+`;
+const PlusIcon = styled(Button)`
+  &:after {
+    content: '+';
+    font-weight: var(--font-weight-bold);
+    font-size: var(--font-size-3);
+  }
+  height: var(--space-6);
+  width: var(--space-6);
+  background-color: var(--sohoLights);
+  color: var(--hemocyanin);
+  border-radius: 33%;
 `;
 const PriceContainer = styled(Text)`
   color: var(--ice);
-  margin-right: var(--space-2);
-`;
-const Container = styled(Box)`
-  display: flex;
-  align-items: center;
-  margin-top: var(--space-4);
+  font-size: var(--font-size-6);
+  font-weight: var(--font-weight-bold);
 `;
 const QuantityContainer = styled(Box)`
   display: flex;
+  flex-direction: column;
   align-items: center;
 `;
 const QuantityTitle = styled(Text)`
-  color: var(--purpleHaze);
-  margin-right: var(--space-2);
+  color: var(--ice);
+  font-size: var(--font-size-2);
 `;
 const QuantityItem = styled(Text)`
   color: var(--ice);
+  font-size: var(--font-size-6);
+  font-weight: var(--font-weight-bold);
+`;
+const StyledButton = styled(Button)`
+  width: 100%;
+  justify-self: stretch;
+  background-color: var(--sohoLights);
+  color: var(--hemocyanin);
+  text-align: center;
+  padding: var(--space-5);
+  align-self: stretch;
+  margin-top: var(--space-4);
+  font-size: var(--font-size-4);
+  font-weight: var(--font-weight-medium);
+  border-radius: var(--radius-5);
 `;
 
 interface AddToCartProps {
   id: number;
+  price: number;
 }
 
 export default function AddToCart({ id, price }: AddToCartProps) {
@@ -48,18 +93,16 @@ export default function AddToCart({ id, price }: AddToCartProps) {
   return (
     <Container>
       <PriceContainer>{price}</PriceContainer>
-      <StyledButton variant="solid" onMouseDown={() => setQuantity(quantity + 1)}>
-        +
-      </StyledButton>
-      <QuantityContainer>
-        <QuantityTitle />
-        <QuantityItem>{quantity}</QuantityItem>
-      </QuantityContainer>
-      <StyledButton variant="solid" onMouseDown={() => setQuantity(quantity - 1)} disabled={quantity === 1}>
-        -
-      </StyledButton>
+      <CounterContainer>
+        <MinusIcon variant="solid" onMouseDown={() => setQuantity(quantity - 1)} disabled={quantity === 1} />
+        <QuantityContainer>
+          <QuantityTitle>Qty</QuantityTitle>
+          <QuantityItem>{quantity}</QuantityItem>
+        </QuantityContainer>
+        <PlusIcon variant="solid" onMouseDown={() => setQuantity(quantity + 1)} />
+      </CounterContainer>
       <StyledButton variant="solid" onClick={handleAddToCart}>
-        Add to Basket
+        Add to cart
       </StyledButton>
     </Container>
   );
